a94784e1c5cc33b88509fb5b97f728be1a9d7c37
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-05 21:28:20 +0200

    isolates: demoing a long running asynchronous task

--- bin/isolates.dart
+++ bin/isolates.dart
@@ -1,8 +1,8 @@
-void main() {
+Future<void> main() async {
   print('isolates');
 
   // Simulate a long-running task.
-  print(playHideAndSeekTheLongVersion());
+  print(await playHideAndSeekTheLongVersionFutures());
 }
 
 // A function that simulates a long-running task.
@@ -13,6 +13,20 @@ String playHideAndSeekTheLongVersion() {
   for (var i = 1; i <= 10000000000; i++) {
     counting = i;
   }
+
+  return '$counting! Ready or not, here I come!';
+}
+
+// An asynchronous version of the long-running task using Future.
+Future<String> playHideAndSeekTheLongVersionFutures() async {
+  var counting = 0;
+
+  // Simulate a long computation by counting to a large number asynchronously.
+  await Future(() {
+    for (var i = 1; i <= 10000000000; i++) {
+      counting = i;
+    }
+  });
 
   return '$counting! Ready or not, here I come!';
 }
