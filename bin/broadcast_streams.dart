882405668c278bea806cf6994e1ee95e24c8f557
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-12-24 12:40:55 +0200

    convert single-subscription stream to broadcast stream

--- bin/broadcast_streams.dart
+++ bin/broadcast_streams.dart
@@ -24,9 +24,35 @@ void runBroadcastStreamWithController() {
   controller.add(3);
 
   // Close the controller
-  controller.close(); // Always close the controller when done to free resources. This will also close the stream.
+  controller
+      .close(); // Always close the controller when done to free resources. This will also close the stream.
+}
+
+void runConvertSingleSubscripptionStreamToBroadcastStream() {
+  // Create a single-subscription stream
+  Stream<int> numbers = Stream.fromIterable(
+    [1, 2, 3],
+  ); // Single-subscription streams: Only one listener allowed. A second listener throws a Bad state: Stream has already been listened to.
+
+  // Convert it into a broadcast stream
+  Stream<int> broadcastNumbers = numbers
+      .asBroadcastStream(); // Broadcast streams: Multiple listeners allowed, each gets the same events.
+
+  // Hot behavior: If you attach listeners after some events have already been emitted,
+  // those late listeners won’t receive past events—they only get future ones.
+
+  // First listener
+  broadcastNumbers.listen((n) {
+    print('Listener 1 got: $n');
+  });
+
+  // Second listener
+  broadcastNumbers.listen((n) {
+    print('Listener 2 got: $n');
+  });
 }
 
 void main() {
-  runBroadcastStreamWithController();
+  // runBroadcastStreamWithController();
+  runConvertSingleSubscripptionStreamToBroadcastStream();
 }
